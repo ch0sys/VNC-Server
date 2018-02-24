ad99f4c9e8cd1553ef43afdf48f64944e1d62134
ch0 <lesserooniti@gmail.com>
2018-02-24 17:15:35 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,16 +4,16 @@
 # Developer: ch0
 # ******************************************
 
-                 sudo apt-get update && sudo apt-get upgrade
-                 sudo apt-get install ubuntu-desktop gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal
-                 sudo apt-get install vnc4server
+                 sudo apt-get update && sudo apt-get upgrade -y
+                 sudo apt-get install ubuntu-desktop gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal -y
+                 sudo apt-get install vnc4server -y 
                  vncserver :1
                  vncserver -kill :1
                  rm -Rf ~/.vnc/xstartup
                  cd ~/.vnc
                  wget https://raw.githubusercontent.com/ch0sys/VNC-Server/master/xstartup
                  clear
-                 sudo apt-get install wine
+                 sudo apt-get install wine -y
                  cd /Downloads
                  wget https://hitleap.com/viewer/download?platform=Linux
                  tar xpvf "HitLeap Viewer.tar.xz"
